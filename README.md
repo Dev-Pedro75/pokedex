cd73f11a967d1d468a8b8b54c9fae4e9aba28b5e
João <mellofontesjp16@gmail.com>
2021-08-28 16:16:09 -0300

    first-commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1 align='center'>Pokedex</h1>
+
+<h2 align='center'>About</h2>
+
+<h3 align='center'>A pokedex with the first 151 pokemons, using <a href="https://pokeapi.co/">pokeApi</a> data</h3>
+
+<img src='img/gif-pokedex.gif'>
+
+## 🚀 Technologies
+
+<ul>
+    <li>Javascript</li>
+    <li>html</li>
+    <li>css</li>
+</ul>
+
+<div align='center'>
+  <h3>My linkedin</h3>
+  <a href="https://www.linkedin.com/in/joao-pedro-mello/" target='_blank'><img src='https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white'/></a>
+</div>
+
+Made by João Pedro
---
+++ app.js
@@ -0,0 +1,72 @@
+const ul = document.querySelector("ul");
+const modal = document.querySelector(".modal");
+const modalOverlay = document.querySelector(".modal-overlay");
+
+function getPokemons() {
+  const PokemonsPromise = [];
+
+  for (let i = 1; i < 152; i++) {
+    PokemonsPromise.push(
+      fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then((response) =>
+        response.json()
+      )
+    );
+  }
+
+  Promise.all(PokemonsPromise).then((pokemons) => {
+    console.log(pokemons[0]);
+    pokemons.forEach(({ name, types, id }) => {
+      ul.innerHTML += `
+            <li class='${types[0].type.name}' onclick='openModal(${id})'>
+                <div class='pokemon-image-container'>
+                    <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id
+                      .toString()
+                      .padStart(3, 0)}.png'/>
+                </div>
+                <div class='pokemon-info-container'>
+                    <p>#${id.toString().padStart(3, 0)}</p>
+                    <h1>${name}</h1>
+                    <h2>${types[0].type.name}</h2>  
+                </div>
+            </li>
+        `;
+    });
+  });
+}
+
+function openModal(id) {
+  modal.innerHTML = "<h1>Loading...</h1>";
+  modalOverlay.style.display = "flex";
+  setTimeout(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((response) => response.json())
+      .then((response) => {
+        modal.innerHTML = `
+      <button onclick='closeModal()'>X</button>
+      <div class='pokemon-image-modal'>
+      <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${response.id
+        .toString()
+        .padStart(3, 0)}.png'/>
+        </div>
+        <div class='pokemon-info-modal'>
+        <p>#${response.id.toString().padStart(3, 0)}</p>
+        <h1>${response.name}</h1>
+        <h2>${response.types[0].type.name}</h2>
+        </div>
+        <div class='pokemon-info-modal-2'>
+        <h2>HP : ${response.stats[0].base_stat}</h2>
+        <h2>Speed : ${response.stats[5].base_stat}</h2>
+        <h2>Attack : ${response.stats[1].base_stat}</h2>
+        <h2>Defense : ${response.stats[2].base_stat}</h2>
+      </div>
+      `;
+      });
+  }, 500);
+}
+
+function closeModal() {
+  modalOverlay.style.display = "none";
+  modal.innerHTML = "";
+}
+
+getPokemons();
